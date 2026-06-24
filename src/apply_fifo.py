ee304e939d7729aba8da36e5dcbf8b8eb1af0ddf
Jon <jon_portugalete@hotmail.com>
2026-06-24 18:46:28 +0200

    Fifo method PnL computing module.

---
+++ src/apply_fifo.py
@@ -0,0 +1,58 @@
+import pandas as pd
+
+def compute_fifo(df: pd.DataFrame) -> pd.DataFrame:
+
+    fifo = deque()
+    results = []
+
+    for _, row in df.iterrows():
+
+        tx_type = row["type"].strip().lower()
+
+        # BUY → add lot
+        if tx_type == "buy":
+            fifo.append({
+                "remaining_vol": row["vol"],
+                "unit_cost": row["real_unit_price"]
+            })
+            continue
+
+        # SELL → consume FIFO
+        if tx_type == "sell":
+
+            remaining = row["vol"]
+            fifo_cost = 0.0
+
+            while remaining > 0:
+
+                if not fifo:
+                    raise ValueError("FIFO empty")
+
+                lot = fifo[0]
+
+                consumed = min(remaining, lot["remaining_vol"])
+
+                fifo_cost += consumed * lot["unit_cost"]
+
+                lot["remaining_vol"] -= consumed
+                remaining -= consumed
+
+                if lot["remaining_vol"] == 0:
+                    fifo.popleft()
+
+            revenue = row["net_amount"]
+            pnl = revenue - fifo_cost
+
+            results.append({
+                "time": row["time"],
+                "sold_vol": row["vol"],
+                "revenue": revenue,
+                "fifo_cost": fifo_cost,
+                "pnl": pnl
+            })
+
+            continue
+
+        raise ValueError(f"Unsupported type: {row['type']}")
+
+    return pd.DataFrame(results)

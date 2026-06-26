63190114b2508eb907755b93570f04cd29cabc35
Jon <jon_portugalete@hotmail.com>
2026-06-26 19:34:57 +0200

    ·Renamed old function names by new ones. ·Added get_pnl_df_from_trades. ·Within get_pnl, either calling get_pnl_df_from_trades or simply get_pnl_df is decided.

--- src/pnl.py
+++ src/pnl.py
@@ -1,10 +1,9 @@
 import pandas as pd
 from utils_log import DataLogger
-from utils_fx import init_fx_cache, save_fx_cache
 from utils_csv import get_df_from_csv, write_df_to_csv
-from extract_asset_data import extract_asset_events
-from gen_columns import gen_clean_events, add_fx_conversion
-from apply_fifo import compute_fifo
+from extract_asset_data import clean_asset_data, clean_asset_data_from_trades
+from gen_columns import gen_data, gen_data_from_trades
+from apply_fifo import compute_fifo, compute_fifo_from_trades
 
 
 _dlog : DataLogger = DataLogger()
@@ -15,25 +14,31 @@ def calc_pnl_from_df(df: pd.DataFrame, year: int) -> float:
     return df.loc[df["time"].dt.year == year, "pnl"].sum()
 
 
-def get_pnl_df(df: pd.DataFrame, asset: str, target_cur: str, year: int, output_csv_path: str, fx_cache_path: str) -> pd.DataFrame:
-    df = extract_asset_events(df, asset)    
-    df = gen_clean_events(df)
-    
-    init_fx_cache(fx_cache_path)
-    df = add_fx_conversion(df, target_cur)
-    save_fx_cache(fx_cache_path)
-    
+def get_pnl_df(df: pd.DataFrame, asset: str, target_cur: str, fx_cache_path: str) -> pd.DataFrame:
+    df = clean_asset_data(df, asset)
+    df = gen_data(df, target_cur, fx_cache_path)
     df = compute_fifo(df)
     
     return df
 
 
-def get_pnl(input_csv_path: str, asset: str, target_cur: str, year: int, output_csv_path: str, fx_cache_path: str) -> int:
+def get_pnl_df_from_trades(df: pd.DataFrame, asset: str, target_cur: str, fx_cache_path: str) -> pd.DataFrame:
+    df = clean_asset_data_from_trades(df, asset)
+    df = gen_data_from_trades(df)
+    df = compute_fifo_from_trades(df, target_cur, fx_cache_path)
+    
+    return df
+
+
+def get_pnl(input_csv_path: str, asset: str, target_cur: str, year: int, output_csv_path: str, fx_cache_path: str, use_trades: bool) -> int:
     _dlog.log_dbg(f"Computing PnL")
     
     df: pd.DataFrame = get_df_from_csv(input_csv_path)
 
-    df = get_pnl_df(df, asset, target_cur, year, output_csv_path, fx_cache_path)
+    if use_trades:
+        df = get_pnl_df_from_trades(df, asset, target_cur, fx_cache_path)
+    else:
+        df = get_pnl_df(df, asset, target_cur, fx_cache_path)
 
     if len(output_csv_path) > 0:
         write_df_to_csv(df, output_csv_path)

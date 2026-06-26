7835d986b541cfa03e3daf405d9b982d51abca2f
Jon <jon_portugalete@hotmail.com>
2026-06-26 16:44:56 +0200

    Fixed typo.

--- src/utils_csv.py
+++ src/utils_csv.py
@@ -5,7 +5,7 @@ def get_df_from_csv(input_file: str) -> pd.DataFrame:
     try:
         df: pd.DataFrame = pd.read_csv(input_file)
     except FileNotFoundError:
-        raise FileNotFoundError(f"Could not fine input file: {input_file}")
+        raise FileNotFoundError(f"Could not find input file: {input_file}")
     except Exception as e:
         raise RuntimeError(f"An error occurred while reading CSV file: {e}")
     
